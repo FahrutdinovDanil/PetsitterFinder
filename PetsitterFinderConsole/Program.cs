7fcea533fc6cf1b5eff09f3b4270ad9b57cc96b1
Даниль Фахрутдинов <danilfahrutdinov357@mail.ru>
2022-05-27 15:37:15 +0300

    added registration console

--- PetsitterFinderConsole/Program.cs
+++ PetsitterFinderConsole/Program.cs
@@ -2,6 +2,7 @@
 using Core.DB;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,18 @@ namespace PetsitterFinderConsole
 {
     public class Program
     {
+        private ObservableCollection<User> users = new ObservableCollection<User>(Connection.connection.Users);
         static void Main(string[] args)
         {
             User user;
+
             bool auth = false;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.BackgroundColor = ConsoleColor.Yellow;
             Console.WriteLine("[+] Добро пожаловать в Petsitter Finder!");
             while (!auth)
             {
-                Console.WriteLine("[+] Для авторизации введите /login, для регистрации /registration, для входа как сотрудник /loginEmp");
+                Console.WriteLine("[+] Для авторизации введите /login, для регистрации /registrationClient, для входа как сотрудник /registrationSitter");
                 string answer = Console.ReadLine();
                 if (answer == "/login")
                 {
@@ -40,50 +43,43 @@ namespace PetsitterFinderConsole
                         Console.WriteLine("[+] Логин или пароль не верный, попробуйте еще раз!");
                     }
                 }
-                //else if (answer == "/registration")
-                //{
-                //    bool correcInput = false;
-                //    while (!correcInput)
-                //    {
-                //        Console.WriteLine("[+] Введите Ваше имя:");
-                //        string usrName = Console.ReadLine();
-                //        Console.WriteLine("[+] Введите Вашу фамилию:");
-                //        string usrSurname = Console.ReadLine();
-                //        Console.WriteLine("[+] Введите Ваш номер телефона :");
-                //        string usrNumber = Console.ReadLine();
-                //        Console.WriteLine("[+] Введите Ваш логин:");
-                //        string usrLogin = Console.ReadLine();
-                //        Console.WriteLine("[+] Введите Ваш пароль:");
-                //        string usrPassword = Console.ReadLine();
-                //        int id_Role = 2;
-                //        if (usrLogin != null && usrName != null && usrSurname != null && usrPassword != null)
-                //        {
-                //            correcInput = true;
-                //            Console.WriteLine("[+] Загрузка...");
-                //            Parent par = new Parent();
-                //            par.Login = usrLogin;
-                //            par.Password = usrPassword;
-                //            par.Name = usrName;
-                //            par.Surname = usrSurname;
-                //            par.Number = usrNumber;
-                //            par.id_Role = id_Role;
-                //            if (MainFunc.CheckRegistrationLogin(par.Login))
-                //            {
-                //                AddToBD.AddParent(par);
-                //                Console.WriteLine($"Приветсвуем Вас, {par.Name}!");
-                //                auth = true;
-                //                MainMenu((int)par.id_Role, par.id_Parent);
-                //                break;
-                //            }
-                //            else
-                //            {
-                //                Console.WriteLine("[+] Пользователь с таким логином уже существует");
-                //            }
+                else if (answer == "/registrationClient")
+                {
+                    bool correcInput = false;
+                    while (!correcInput)
+                    {
+                        Console.WriteLine("[+] Введите Ваше имя:");
+                        string clientName = Console.ReadLine();
+                        Console.WriteLine("[+] Введите Вашу фамилию:");
+                        string clientSurname = Console.ReadLine();
+                        Console.WriteLine("[+] Введите Ваш номер телефона :");
+                        string clientNumber = Console.ReadLine();
+                        Console.WriteLine("[+] Введите Ваш логин:");
+                        string clientLogin = Console.ReadLine();
+                        Console.WriteLine("[+] Введите Ваш пароль:");
+                        string clientPassword = Console.ReadLine();
 
-                //        }
+                        if (clientName != null && clientSurname != null && clientNumber != null && clientLogin != null && clientPassword != null)
+                        {
+                            User userToAdd = new User();
+                            userToAdd.Password = clientPassword;
+                            userToAdd.Login = clientLogin;
+                            userToAdd.RoleId = 3;
+                            Registration.AddUser(userToAdd);
 
-                //    }
-                //}
+                            Client clientToAdd = new Client();
+                            clientToAdd.Name = clientName;
+                            clientToAdd.Surname = clientSurname;
+                            clientToAdd.Phone = clientNumber;
+                            clientToAdd.UserId = userToAdd.Id;
+                            correcInput = true;
+                            Registration.AddClient(clientToAdd);
+                            Console.WriteLine("[+] Загрузка...");
+
+                        }
+
+                    }
+                }
             }
         }
     }
